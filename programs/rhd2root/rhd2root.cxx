25efb3ef01d285644014ff8463be83eebe4a6fab
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2019-05-17 13:33:56 +0100

    Update rhd2root converter
    - Fix linking issues due to project name change
    - Factorize conversion into a function template on the stream type
    - Support calo, tracker, trigger stream types
    - Remove intermediate unsorted (by trigger id) root file
    - Time conversion and sort processes

--- programs/rhd2root/rhd2root.cxx
+++ programs/rhd2root/rhd2root.cxx
@@ -3,31 +3,137 @@
 // - Boost:
 #include <boost/program_options.hpp>
 
+#include <cstdio>
 #include <iostream>
 
 #include "snfee/data/calo_hit_record.h"
+#include "snfee/data/tracker_hit_record.h"
+#include "snfee/data/trigger_record.h"
 #include "snfee/io/multifile_data_reader.h"
 
 // Input is 1-N RTD file to convert
 // Output is 1 root file
-#include "TROOT.h"
 #include "TFile.h"
+#include "TROOT.h"
 #include "TTree.h"
 #include "TTreeIndex.h"
 
 #include "ROOT/TSeq.hxx"
 #include "TRandom.h"
+#include "TStopwatch.h"
 
 namespace {
   // Quick and dumb mess-up of trigger ids
-  int32_t mixTriggerID(int32_t t) {
-    int32_t nt = t + static_cast<int32_t>(gRandom->Gaus(0,10));
+  int32_t
+  mixTriggerID(int32_t t)
+  {
+    int32_t nt = t + static_cast<int32_t>(gRandom->Gaus(0, 10));
     return nt < 0 ? 0 : nt;
   }
-}
 
+  // Convert a Boost stream of RHD records to a TTree, with sorting of entries
+  // by trigger_id
+  template <typename RHDType>
+  void
+  rhd2root(snfee::io::multifile_data_reader& reader,
+           std::string const& ofilename)
+  {
+    // Parameters (for future factorization)
+    const std::string unsortedFilename{ofilename + ".unsorted"};
+    // Use gsl finally once C++17...
+    const std::string sortedFilename{ofilename};
 
-int main(int argc, char *argv[])
+    const std::string rhdFileTitle{"SuperNEMO RHD File"};
+    const std::string rhdTreeName{"RawHitData"};
+    const std::string rhdTreeTitle{"SuperNEMO Raw Hit Data"};
+    const std::string rhdBranchName{"RHD"};
+    const std::string rhdTriggerIDBranchName{"_trigger_id_"};
+
+    // Intermediate direct conversion
+    {
+      TStopwatch tsp;
+      tsp.Start();
+      TFile unsortedFile{unsortedFilename.c_str(), "RECREATE"};
+      TTree rhdTree{rhdTreeName.c_str(), rhdTreeTitle.c_str()};
+
+      auto rhd = new RHDType{};
+      rhdTree.Branch(rhdBranchName.c_str(), &rhd);
+
+      std::string msg{"Converting RHD stream to ROOT Tree, done: "};
+
+      size_t counter{0};
+      while (reader.has_record_tag()) {
+        reader.load(*rhd);
+
+        // Mix up to model non-time ordering
+        rhd->set_trigger_id(mixTriggerID(rhd->get_trigger_id()));
+
+        rhdTree.Fill();
+
+        if (!(counter % 1000)) {
+          std::clog << msg << counter << "\r";
+        }
+        counter++;
+      }
+      std::clog << msg << counter << "\n";
+
+      unsortedFile.Write();
+      tsp.Stop();
+      tsp.Print();
+    }
+
+    // Sort entries by trigger_id
+    {
+      TStopwatch tsp;
+      tsp.Start();
+
+      ROOT::EnableImplicitMT();
+
+      TFile unsortedFile{unsortedFilename.c_str()};
+      TTree* unsortedTree;
+      unsortedFile.GetObject(rhdTreeName.c_str(), unsortedTree);
+      // Help random access performance, can expect same trigger_ids to
+      // be close to each other
+      // To be adjusted based on typical RHD file size (or from GetNbytes)
+      unsortedTree->SetMaxVirtualSize(1e9);
+
+      const auto nentries = unsortedTree->BuildIndex(rhdTriggerIDBranchName.c_str());
+      auto index = dynamic_cast<TTreeIndex*>(unsortedTree->GetTreeIndex());
+
+      RHDType* hit{nullptr};
+      unsortedTree->SetBranchAddress(rhdBranchName.c_str(), &hit);
+
+      TFile sortedFile{sortedFilename.c_str(), "RECREATE", rhdFileTitle.c_str()};
+      auto sortedTree = unsortedTree->CloneTree(0);
+
+      std::string msg{"Sorting RHD Tree by Trigger ID: "};
+
+      for (auto i : ROOT::TSeqI(nentries)) {
+        unsortedTree->GetEntry(index->GetIndex()[i]);
+        sortedTree->Fill();
+        if (!(i % 1000)) {
+          std::clog << msg << 100 * i / nentries << "%\r";
+        }
+      }
+      std::clog << msg << "100%\n";
+
+      sortedFile.Write();
+      tsp.Stop();
+      tsp.Print();
+    }
+
+    // Remove intermediate file (eventually handled automatically
+    // by gsl finally)
+    int rmStatus = std::remove(unsortedFilename.c_str());
+    if (rmStatus) {
+      std::perror(("Failed to remove intermediate ROOT file '" +
+                  unsortedFilename + "'").c_str());
+    }
+  }
+} // namespace
+
+int
+main(int argc, char* argv[])
 {
   // - Command Line
   snfee::io::multifile_data_reader::config_type inputConfig;
@@ -35,26 +141,21 @@ int main(int argc, char *argv[])
 
   namespace po = boost::program_options;
   po::options_description opts("Allowed options");
-  opts.add_options()
-    ("help,h", "produce help message")
-    ("input-file,i",
-      po::value<std::vector<std::string>>(&inputConfig.filenames)
+  opts.add_options()("help,h", "produce help message")(
+    "input-file,i",
+    po::value<std::vector<std::string>>(&inputConfig.filenames)
       ->value_name("<PATH>")
       ->required()
       ->multitoken(),
-      "path to RHD input file")
-    ("output-file,o",
-      po::value<std::string>(&outputFile)
-      ->value_name("<PATH>")
-      ->required(),
-      "path to ROOT output file");
+    "path to RHD input file")(
+    "output-file,o",
+    po::value<std::string>(&outputFile)->value_name("<PATH>")->required(),
+    "path to ROOT output file");
 
   // Describe command line arguments
   try {
     po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv)
-              .options(opts)
-              .run(), vm);
+    po::store(po::command_line_parser(argc, argv).options(opts).run(), vm);
     if (vm.count("help")) {
       std::cout << opts << "\n";
       return 0;
@@ -68,81 +169,26 @@ int main(int argc, char *argv[])
 
   // - Processing
   // 1. Determine type of input RHD stream (calo, tracker, trigger)
-  {
+  try {
     snfee::io::multifile_data_reader reader{inputConfig};
     std::string rhdType{reader.get_record_tag()};
-    std::clog << "Input stream is type '" << rhdType << "'" << std::endl;;
-  }
 
-  // Assume stream is pure
-  // Will have a function to pass reader.get_record_tag() to
-  using RHDType = snfee::data::calo_hit_record;
-
-  // 2. Convert
-  {
-    // RHD Boost stream
-    snfee::io::multifile_data_reader reader{inputConfig};
-
-    // ROOT output
-    TFile writer{outputFile.c_str(), "RECREATE"};
-    TTree rhdTree{"RawHitData", "SuperNEMO Raw Hit Data"};
-
-    auto rhd = new RHDType{};
-    rhdTree.Branch("RHD", &rhd);
-
-    size_t counter{0};
-    std::string msg {"Converting RHD stream to ROOT Tree, done: "};
-    while(reader.has_record_tag()) {
-      reader.load(*rhd);
-
-      // Mix up to model non-time ordering
-      rhd->set_trigger_id(mixTriggerID(rhd->get_trigger_id()));
-
-      rhdTree.Fill();
-
-      if (! (counter % 1000)) std::clog << msg << counter << "\r";
-      counter++;
+    // 2. Process according to stream type
+    if (rhdType == snfee::data::calo_hit_record::SERIAL_TAG) {
+      rhd2root<snfee::data::calo_hit_record>(reader, outputFile);
+    } else if (rhdType == snfee::data::tracker_hit_record::SERIAL_TAG) {
+      rhd2root<snfee::data::tracker_hit_record>(reader, outputFile);
+    } else if (rhdType == snfee::data::trigger_record::SERIAL_TAG) {
+      rhd2root<snfee::data::tracker_hit_record>(reader, outputFile);
+    } else {
+      std::cerr << "Unknown RHD type '" << rhdType << "' in input stream "
+          << std::endl;
+      return EXIT_FAILURE;
     }
-    std::clog << msg << counter << "\n";
-
-    // Can print/output tree stats here if required
-    //rhdTree.Print("RHD");
-    writer.Write();
   }
-
-  // 3. Sort by trigger_id
-  {
-    ROOT::EnableImplicitMT();
-
-    TFile oldFile{outputFile.c_str()};
-    TTree* oldTree;
-    oldFile.GetObject("RawHitData", oldTree);
-    // Help random access performance, can expect same trigger_ids to
-    // be close to each other
-    // To be adjusted based on typical RHD file size
-    oldTree->SetMaxVirtualSize(1e9);
-
-    const auto nentries = oldTree->BuildIndex("_trigger_id_");
-    TTreeIndex* index = (TTreeIndex*)oldTree->GetTreeIndex();
-
-    RHDType* hit{nullptr};
-    oldTree->SetBranchAddress("RHD", &hit);
-
-    TFile newFile{"sorted.root", "RECREATE"};
-    auto newTree = oldTree->CloneTree(0);
-
-    std::string msg{"Sorting RHD Tree by Trigger ID: "};
-
-    for (auto i : ROOT::TSeqI(nentries)) {
-      oldTree->GetEntry(index->GetIndex()[i]);
-      newTree->Fill();
-      if (! (i % 1000)) std::clog << msg << 100*i/nentries << "%\r";
-    }
-    std::clog << msg << "100%\n";
-
-    // Can print/output tree stats here if required
-    // newTree->Print();
-    newFile.Write();
+  catch(std::exception& e) {
+    std::cerr << "RHD2ROOT Conversion threw an exception: " << e.what() << std::endl;
+    return EXIT_FAILURE;
   }
 
   return 0;
